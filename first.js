2231fd0717334866957c7ffa9f5a60c5dfd7c43a
saketdokwal <suyashs097@gmail.com>
2024-10-28 16:00:26 +0530

    initail commit

---
+++ first.js
@@ -0,0 +1,45 @@
+// Primitive data types
+// Javascript supports various data types:
+
+// Number: Integer, floats
+
+// String : text enclose in single or double quotes.
+
+// Boolean: 'true', 'false'.
+
+// null
+
+// undefined
+
+// variable declaration
+let fname = "Saket";
+console.log(fname);
+const age = 30;
+console.log(age);
+
+var isBatchmate = "true";
+console.log(isBatchmate);
+
+// Non primitive datatypes
+// Array: ordered collection of elements
+
+// Object: collection of key-value pairs.
+
+// Array
+let friendsName = ["Namit", "Aakash", "Pulkit", "Ankit"];
+console.log(friendsName);
+console.log(friendsName[2]);
+//OBJECT
+let personalDetail = {
+  name: "Saket",
+  age: 26,
+  qualification: "graduate in computer science",
+};
+console.log(personalDetail);
+
+// Hoising example in js
+
+console.log(myAge);
+
+var myAge = 26;
+console.log(myAge);
